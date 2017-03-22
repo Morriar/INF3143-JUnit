01cb452610a0b991491b0060383a24d3be75e037
Alexandre Terrasa <alexandre@moz-code.org>
2017-03-21 21:55:27 -0400

    Add random seeded example
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

---
+++ src/rand/RandomNumber.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 Alexandre Terrasa <alexandre@moz-code.org>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package rand;
+
+import java.util.Random;
+
+public class RandomNumber {
+
+    Random gen;
+
+    public RandomNumber() {
+        gen = new Random(System.currentTimeMillis());
+    }
+
+    public RandomNumber(Integer seed) {
+        gen = new Random(seed);
+    }
+
+    public Integer getRandom() {
+        return gen.nextInt();
+    }
+
+}
